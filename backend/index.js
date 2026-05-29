4a2b39b5c2d3c10a1702b4cc920d65f31331bbda
vivekydv936 <vivekydv936@gmail.com>
2026-05-29 16:27:53 +0530

    Fix Vercel serverless cold start DB timeouts

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const app = require('./src/app');
 const connectDB = require('./src/config/db');
 
-// Connect to MongoDB
-// Note: In serverless environments, this will run on cold start.
-connectDB().catch(console.dir);
-
-// Export the Express API
-module.exports = app;
+// Connect to MongoDB and export a serverless handler
+// This ensures the database is connected BEFORE Express tries to process the request
+module.exports = async (req, res) => {
+  await connectDB();
+  return app(req, res);
+};
